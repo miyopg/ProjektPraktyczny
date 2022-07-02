0f1b139a4b1a1cda668eb9e2b89f067fa1cded11
Grzesiek <grzesiek138@onet.eu>
2022-07-02 15:31:17 +0200

    client rework

--- src/main/java/pl/sda/project/shop/model/Client.java
+++ src/main/java/pl/sda/project/shop/model/Client.java
@@ -20,17 +20,17 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
-    @Column(name = "first_name", nullable = false, length = 32 )
+    @Column(name = "firstName", nullable = false, length = 32 )
     protected String firstName;
-    @Column(name = "last_name", nullable = false, length = 32, insertable = false, updatable = false)
+    @Column(name = "lastName", nullable = false, length = 32)
     protected String lastName;
     @Column(name = "email", nullable = false, length = 64, unique = true)
     protected String email;
-    @Column(name = "phone_number", nullable = false, length = 9, unique = true)
+    @Column(name = "phoneNumber", nullable = false, length = 9, unique = true)
     protected String phoneNumber;
-    @Column(name = "last_name", nullable = false, length = 64 )
+    @Column(name = "street", nullable = false, length = 64 )
     protected String street;
-    @Column(name = "post_code", nullable = false, length = 6 )
+    @Column(name = "postCode", nullable = false, length = 6 )
     protected String postCode;
     @Column(name = "city", nullable = false, length = 64 )
     protected String city;
